185eaba13b5ef447ea61521e5ffbb9ffb4fcc974
Dominik Blatzheim <8586904+PragmaticBeaver@users.noreply.github.com>
2021-08-04 14:30:49 +0200

    feat: observe D&D Beyond DOM for changes

--- chrome-extension/dndbeyond-injection.js
+++ chrome-extension/dndbeyond-injection.js
@@ -1,9 +1,23 @@
 console.log("=>> I was injected!");
+console.log(document);
 
 /**
  * todo
- * receive msg's => document.addEventListener
- * send msg's => document.dispatchEvent
+ *  receive msg's => document.addEventListener
+ *  send msg's => document.dispatchEvent
  */
 
-console.log(document);
+function onDOMChanged(mutation, observer) {
+  console.log("DOM changed;");
+  console.log(mutation, observer);
+}
+
+// observe document for DOM changes
+const observer = new window.MutationObserver(onDOMChanged);
+observer.observe(document, {
+  subtree: true,
+  childList: true,
+  characterData: true,
+});
+
+// todo observer.disconnect(); ?
