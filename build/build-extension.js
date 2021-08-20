fbcd9c2f887db6dd074bccf95ec1a6a1781abaf7
Dominik Blatzheim <8586904+PragmaticBeaver@users.noreply.github.com>
2021-08-20 10:01:14 +0200

    chore: build valid extension inside dist/extension

--- build/build-extension.js
+++ build/build-extension.js
@@ -1,14 +1,20 @@
 import { build } from "esbuild";
 import fs from "fs";
 
+// construct valid filepaths
 const rootPath = "./chrome-extension/";
 let every = fs.readdirSync(rootPath).filter((src) => src.endsWith(".js"));
 every = every.map((dir) => {
   return rootPath + dir;
 });
 
+// build all
+const outDir = "./dist/extension";
 build({
   entryPoints: every,
   bundle: false,
-  outdir: "./dist/extension",
+  outdir: outDir,
 }).catch(() => process.exit(1));
+
+// copy manifest
+fs.copyFileSync(rootPath + "manifest.json", outDir + "/manifest.json");
