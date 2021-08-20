6de5baee8f8cbcb26422abed616aacacef74367a
Dominik Blatzheim <8586904+PragmaticBeaver@users.noreply.github.com>
2021-08-20 11:29:41 +0200

    refactor: background will remember current port connections
    and handle disconnections

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,28 +1,56 @@
 console.log("hello from background.js");
 
-const FOUNDRYVTT_URL = "*://*/game";
-const DNDBEYOND_CHARACTER_URL = "*://*.dndbeyond.com/*characters/*";
-const DNDBEYOND_MONSTER_URL = "*://*.dndbeyond.com/monsters/*";
-const DNDBEYOND_ENCOUNTER_URL = "*://*.dndbeyond.com/encounters/*";
-const DNDBEYOND_ENCOUNTERS_URL = "*://*.dndbeyond.com/my-encounters";
-const DNDBEYOND_COMBAT_URL = "*://*.dndbeyond.com/combat-tracker/*";
-const DNDBEYOND_SPELL_URL = "*://*.dndbeyond.com/spells/*";
-const DNDBEYOND_VEHICLE_URL = "*://*.dndbeyond.com/vehicles/*";
-const DNDBEYOND_SOURCES_URL = "*://*.dndbeyond.com/sources/*";
-const DNDBEYOND_CLASSES_URL = "*://*.dndbeyond.com/classes/*";
+// const FOUNDRYVTT_URL = "*://*/game";
+// const DNDBEYOND_CHARACTER_URL = "*://*.dndbeyond.com/*characters/*";
+// const DNDBEYOND_MONSTER_URL = "*://*.dndbeyond.com/monsters/*";
+// const DNDBEYOND_ENCOUNTER_URL = "*://*.dndbeyond.com/encounters/*";
+// const DNDBEYOND_ENCOUNTERS_URL = "*://*.dndbeyond.com/my-encounters";
+// const DNDBEYOND_COMBAT_URL = "*://*.dndbeyond.com/combat-tracker/*";
+// const DNDBEYOND_SPELL_URL = "*://*.dndbeyond.com/spells/*";
+// const DNDBEYOND_VEHICLE_URL = "*://*.dndbeyond.com/vehicles/*";
+// const DNDBEYOND_SOURCES_URL = "*://*.dndbeyond.com/sources/*";
+// const DNDBEYOND_CLASSES_URL = "*://*.dndbeyond.com/classes/*";
+
+const DNDBEYOND_PORT_ID = "dndbeyond-sync";
+const FOUNDRY_PORT_ID = "foundry-sync";
+const PORTS = {};
+
+function cachePort(port) {
+  console.log("adding port", port.name);
+  PORTS[port.name] = port;
+}
+
+function removePort(port) {
+  console.log("removing port", port.name);
+  delete PORTS[port.name];
+}
 
 function handleBeyondConnection(port) {
   port.onMessage.addListener((msg) => {
-    // todo
     console.log("msg from beyond", msg);
+    const foundryPort = PORTS[FOUNDRY_PORT_ID];
+    foundryPort.postMessage(msg);
   });
+
+  port.onDisconnect.addListener(() => {
+    removePort(port);
+  });
+
+  cachePort(port);
 }
 
 function handleFoundryConnection(port) {
   port.onMessage.addListener((msg) => {
-    // todo
     console.log("msg from foundry", msg);
+    const beyondPort = PORTS[DNDBEYOND_PORT_ID];
+    beyondPort.postMessage(msg);
   });
+
+  port.onDisconnect.addListener(() => {
+    removePort(port);
+  });
+
+  cachePort(port);
 }
 
 // listen for connections
@@ -30,10 +58,10 @@ chrome.runtime.onConnect.addListener((port) => {
   console.log("port", port);
 
   switch (port.name) {
-    case "dndbeyond-sync":
+    case DNDBEYOND_PORT_ID:
       handleBeyondConnection(port);
       break;
-    case "foundry-sync":
+    case FOUNDRY_PORT_ID:
       handleFoundryConnection(port);
       break;
     default:
