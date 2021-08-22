c146f6768f01e8e603b8f592d80f67a9f5443755
Dominik Blatzheim <8586904+PragmaticBeaver@users.noreply.github.com>
2021-08-22 16:40:14 +0200

    docs: moved todos to new location

--- chrome-extension/foundry-injection.js
+++ chrome-extension/foundry-injection.js
@@ -1,4 +1,4 @@
-import { EVENT_TO_FOUNDRY } from "./common.js";
+import { EVENT_FROM_FOUNDRY, EVENT_TO_FOUNDRY } from "./common.js";
 
 console.log("=>> foundry - I was injected!");
 
@@ -48,3 +48,14 @@ observer.observe(document, {
 });
 
 listenForIncomingEvents();
+
+/**
+ * todo - there are 2 options to proceed
+ *  =>  throw events inside DOM and write Foundry plugin to handle these events
+ *      =>> PRO - less effected by HTML chagnes inside foundry
+ *      =>> CON - 2 seperate code-bases which need to be compatible with each other
+ *  =>  read and mutate DOM to acchive results (like making ability checks)
+ *      =>> PRO - less code split because of one code base
+ *      =>> CON - more effected by HTML changes inside foundry
+ *      =>> CON - unsure how to handle item addition to inventory (drag and drop actions)
+ */
