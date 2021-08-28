489a3e10d23e2b3f0b076f6260c233404e62997d
Dominik Blatzheim <8586904+PragmaticBeaver@users.noreply.github.com>
2021-08-28 20:35:32 +0200

    refactor: extracted settings menu code from main.js

--- foundryvtt-plugin/scripts/main.js
+++ foundryvtt-plugin/scripts/main.js
@@ -1,7 +1,6 @@
 import { getPCSheetIds } from "./actor.js";
 import { listenForIncomingEvents, notify } from "./communication.js";
-import { isGMInstance } from "./common.js";
-import { CHARACTER_URLS, getFlag, setFlag } from "./data-flags.js";
+import { injectSettingsMenu } from "./settings-menu.js";
 
 Hooks.on("init", () => {
   console.log("dndbeyond-sync | initalizing ...");
@@ -14,7 +13,7 @@ Hooks.on("ready", () => {
   // todo sync between foundry and beyond
   //  ==>>  check for changes
   //  ==>>  show dialog to user and ask which side to sync (use Beyond data / use Foundry data)
-  injectPCsheetOptionsBtn(getPCSheetIds());
+  injectSettingsMenu(getPCSheetIds());
   listenForIncomingEvents();
 });
 
@@ -80,95 +79,3 @@ Hooks.on("updateCompendium", (pack, change, options, userId) => {
 Hooks.on("renderDocument", (...args) => {
   console.log("renderDocument", args);
 });
-
-/**
- * Injects the dndbeyond-sync Button into each PC Sheet.
- */
-function injectPCsheetOptionsBtn(sheetIds) {
-  if (!isGMInstance()) {
-    console.log("PC instance, skipping options button ...");
-    return;
-  }
-
-  for (const id of sheetIds) {
-    Hooks.on("render" + id, (app, html, data) => {
-      // custom icon
-      const icon = document.createElement("i");
-      icon.classList = "fab fa-d-and-d-beyond";
-
-      // custom button
-      const btn = document.createElement("button");
-      btn.classList = "dndbeyondSync-menu-btn";
-      btn.appendChild(icon);
-      btn.onclick = () => {
-        console.log("configure actor clicked!");
-        openActorConfigDialog();
-      };
-
-      // custom div for flex-row
-      const container = document.createElement("div");
-      container.classList = "dndbeyondSync-menu";
-      container.appendChild(btn);
-
-      // vanilla pc name container
-      const plainHtml = html.get(0);
-      const nameContainer = plainHtml.getElementsByClassName("charname")[0]; //h1
-      nameContainer.classList = "dndbeyondSync-pc-name";
-
-      // move PC name-input into div
-      const nameInput = nameContainer.getElementsByTagName("input")[0];
-      container.appendChild(nameInput);
-
-      nameContainer.appendChild(container);
-    });
-  }
-}
-
-/**
- * todo
- *  => find a way to destinguish events (events are always meant for specific PC / GM)
- *  => idea: save beyond URL (including PC ID) inside Foundry PC sheet
- */
-
-function openActorConfigDialog() {
-  const url = getFlag(game.user, CHARACTER_URLS) || "";
-
-  /**
-   * HTMLElement.outerHTML does't work for input.value!
-   * => because of this, a string representation is necessary
-   */
-  const content =
-    `
-    <div>
-      <input id="dndbeyondSync-pc-sync-input" type="text" placeholder="D&D Beyond URL ..." value="` +
-    url +
-    `">
-    </div>
-  `;
-
-  let d = new Dialog({
-    title: "D&D Beyond Sync - Configure PC",
-    content,
-    buttons: {
-      done: {
-        icon: '<i class="fas fa-save"></i>',
-        label: "Done",
-        callback: () => {
-          console.log("clicked done");
-          const url = document.getElementById(
-            "dndbeyondSync-pc-sync-input"
-          ).value;
-
-          setFlag(game.user, CHARACTER_URLS, url);
-        },
-      },
-    },
-    default: "done",
-    render: (html) =>
-      console.log("onRender - Register interactivity in the rendered dialog"),
-    close: (html) => {
-      console.log("closed dialog");
-    },
-  });
-  d.render(true);
-}
---
+++ foundryvtt-plugin/scripts/settings-menu.js
@@ -0,0 +1,96 @@
+import { isGMInstance } from "./common.js";
+import { CHARACTER_URLS, getFlag, setFlag } from "./data-flags.js";
+
+/**
+ * Dynamically injects the dndbeyond-sync settings menu into each PC ActorSheet as soon as render Hook is called.
+ * @param {string[]} sheetIds ActorSheet ID's of player characters
+ * @returns {boolean}
+ */
+export function injectSettingsMenu(sheetIds) {
+  if (!isGMInstance()) {
+    console.log("PC instance, skipping options button ...");
+    return;
+  }
+
+  for (const id of sheetIds) {
+    Hooks.on("render" + id, (_app, html, _data) => {
+      // custom icon
+      const icon = document.createElement("i");
+      icon.classList = "fab fa-d-and-d-beyond";
+
+      // custom button
+      const btn = document.createElement("button");
+      btn.classList = "dndbeyondSync-menu-btn";
+      btn.appendChild(icon);
+      btn.onclick = () => {
+        console.log("configure actor clicked!");
+        openActorConfigDialog();
+      };
+
+      // custom div for flex-row
+      const container = document.createElement("div");
+      container.classList = "dndbeyondSync-menu";
+      container.appendChild(btn);
+
+      // vanilla pc name container
+      const plainHtml = html.get(0);
+      const nameContainer = plainHtml.getElementsByClassName("charname")[0]; //h1
+      nameContainer.classList = "dndbeyondSync-pc-name";
+
+      // move PC name-input into div
+      const nameInput = nameContainer.getElementsByTagName("input")[0];
+      container.appendChild(nameInput);
+
+      nameContainer.appendChild(container);
+    });
+  }
+}
+
+/**
+ * todo
+ *  => find a way to destinguish events (events are always meant for specific PC / GM)
+ *  => idea: save beyond URL (including PC ID) inside Foundry PC sheet
+ */
+
+function openActorConfigDialog() {
+  const url = getFlag(game.user, CHARACTER_URLS) || "";
+
+  /**
+   * HTMLElement.outerHTML does't work for input.value!
+   * => because of this, a string representation is necessary
+   */
+  const content =
+    `
+      <div>
+        <input id="dndbeyondSync-pc-sync-input" type="text" placeholder="D&D Beyond URL ..." value="` +
+    url +
+    `">
+      </div>
+    `;
+
+  let d = new Dialog({
+    title: "D&D Beyond Sync - Configure PC",
+    content,
+    buttons: {
+      done: {
+        icon: '<i class="fas fa-save"></i>',
+        label: "Done",
+        callback: () => {
+          console.log("clicked done");
+          const url = document.getElementById(
+            "dndbeyondSync-pc-sync-input"
+          ).value;
+
+          setFlag(game.user, CHARACTER_URLS, url);
+        },
+      },
+    },
+    default: "done",
+    render: (html) =>
+      console.log("onRender - Register interactivity in the rendered dialog"),
+    close: (html) => {
+      console.log("closed dialog");
+    },
+  });
+  d.render(true);
+}
