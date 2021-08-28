37a08892fcd3bb79f4d40f75208ae946d0d961d5
Dominik Blatzheim <8586904+PragmaticBeaver@users.noreply.github.com>
2021-08-28 12:12:25 +0200

    chore: gather PC actor/actorSheet on beyond msg

--- foundryvtt-plugin/scripts/communication.js
+++ foundryvtt-plugin/scripts/communication.js
@@ -17,7 +17,38 @@ export function notify(blob) {
  */
 export function listenForIncomingEvents() {
   document.addEventListener(EVENT_TO_FOUNDRY, (...args) => {
-    console.log("received args", args);
-    // todo mutate VTT
+    for (const customEvt of args) {
+      const evt = customEvt.detail;
+      console.log("=>> received", evt);
+
+      // todo is for current user?
+      console.log(CONFIG);
+      console.log(game); //game.user
+      const user = game.users.get(game.userId);
+      console.log("user", user);
+      const charId = user.data.character;
+      console.log("char", charId);
+
+      const actor = game.actors.get(charId);
+      console.log("actor", actor);
+      const sheet = actor.sheet;
+      console.log("sheet", sheet);
+
+      // todo handle GM
+
+      // todo mutate VTT
+
+      // ability-roll
+      if (evt.ability) {
+        console.log("ability-roll", evt.ability);
+        // todo roll d20 => CONFIG.Dice.D20Roll
+        // todo roll damage => CONFIG.Dice.DamageRoll
+      }
+
+      // skill-roll
+      if (evt.skill) {
+        console.log("skill-roll", evt.skill);
+      }
+    }
   });
 }
