3ebe68526edf9e23751a659cda2208435bb52f0d
Dominik Blatzheim <8586904+PragmaticBeaver@users.noreply.github.com>
2021-08-28 22:37:15 +0200

    docs: documented function

--- foundryvtt-plugin/scripts/common.js
+++ foundryvtt-plugin/scripts/common.js
@@ -8,6 +8,10 @@ export const EVENT_TO_FOUNDRY = "dndbeyond-sync-to-foundry";
 // data
 export const CHARACTER_URLS = "charUrls";
 
+/**
+ * Checks if current FoundryVTT instance is GM.
+ * @returns {boolean} is GM
+ */
 export function isGMInstance() {
   return game.user.isGM;
 }
