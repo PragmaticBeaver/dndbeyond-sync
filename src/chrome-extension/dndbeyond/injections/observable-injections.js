eb50a3242ad1f7dc8af36b45e6af846f9966cc0a
Dominik Blatzheim <8586904+PragmaticBeaver@users.noreply.github.com>
2021-09-10 12:10:25 +0200

    refactor: simplified observable mutation handling

--- src/chrome-extension/dndbeyond/injections/observable-injections.js
+++ src/chrome-extension/dndbeyond/injections/observable-injections.js
@@ -5,27 +5,16 @@ import {
 import { injectHpSummary, injectHpManager } from "./inject-hp.js";
 import { injectPreferencesPane } from "./inject-preferences-pane.js";
 
-// mutations
-const HP_SUMMARY = "hp-summary";
-const DEATH_SAVE_PANE = "ds-pane";
-const PREFERENCE_PANE = "pref-pane";
-// complex mutations
-const HP_MUT_PANE = "hp-pane";
-
 // todo idea - simple attach callback to "change HP"-btn's; onclick => collect current value and notify!
 
 export function observeDocument(doc) {
   const observer = new window.MutationObserver((mutations, _observer) => {
-    const mutationsToHandle = {}; // cache for mutations that need to be handled
-
     for (const m of mutations) {
       const element = m.target;
 
       // console.log("m", m);
       // console.log("target/element", element);
       // console.log("element?.className", element?.className);
-      // console.log("element?.parentElement", element?.parentElement);
-      // console.log("element?.parentNode", element?.parentNode);
 
       // side pane - health-manager rendered
       const isHpSidePane =
@@ -33,14 +22,14 @@ export function observeDocument(doc) {
         element?.parentElement?.className?.trim() ===
           "ct-health-manager__adjuster-new-value";
       if (isHpSidePane) {
-        mutationsToHandle[HP_MUT_PANE] = m;
+        injectHpManager(doc);
       }
 
       // hp summary rendered
       const isHpSummary =
         element?.className?.trim() === "ct-health-summary__hp";
       if (isHpSummary) {
-        mutationsToHandle[HP_SUMMARY] = m;
+        injectHpSummary(doc);
       }
 
       // following mutations need function 'getElementsByClassName'
@@ -55,7 +44,7 @@ export function observeDocument(doc) {
         "ct-health-manager__deathsaves-groups"
       )[0];
       if (isDeathSavePane) {
-        mutationsToHandle[DEATH_SAVE_PANE] = m;
+        injectDeathSavePane(doc);
       }
 
       // preference pane rendered
@@ -66,28 +55,7 @@ export function observeDocument(doc) {
         "ddbc-toggle-field  ddbc-toggle-field--is-enabled ddbc-toggle-field--is-interactive"
       )[0];
       if (inactiveToggle || activeToggle) {
-        mutationsToHandle[PREFERENCE_PANE] = m;
-      }
-    }
-
-    console.log("mutationsToHandle", Object.keys(mutationsToHandle));
-    for (const k of Object.keys(mutationsToHandle)) {
-      switch (k) {
-        case HP_MUT_PANE:
-          injectHpManager(doc);
-          break;
-        case HP_SUMMARY:
-          injectHpSummary(doc);
-          break;
-        case DEATH_SAVE_PANE:
-          injectDeathSavePane(doc);
-          break;
-        case PREFERENCE_PANE:
-          injectPreferencesPane(doc);
-          break;
-        default:
-          console.warn("unknown mutation", k);
-          break;
+        injectPreferencesPane(doc);
       }
     }
   });
