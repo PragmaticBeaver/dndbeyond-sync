829abe0d718878fa76dff9e88f9464e71cd3e73f
PragmaticBeaver <dominik.blatzheim@mailbox.org>
2021-11-16 13:15:17 +0100

    feat: refresh D&D Beyond and FoundryVTT websites using extension popup

--- src/chrome-extension/popup.js
+++ src/chrome-extension/popup.js
@@ -1,2 +1,27 @@
 const changeColor = document.getElementById("changeColor");
 changeColor.style.backgroundColor = "red";
+
+const refreshBtn = document.getElementById("refresh-btn");
+refreshBtn.onclick = () => refreshInjections();
+
+const refreshInjections = () => {
+  // get by url "https://www.dndbeyond.com/*"
+  chrome.tabs.query({ url: "https://www.dndbeyond.com/*" }, (tabs) => {
+    console.log("beyond");
+    console.log(tabs);
+    for (const t of tabs) {
+      chrome.tabs.update(t.id, { url: t.url });
+    }
+  });
+
+  // get by Title "Foundry Virtual Tabletop"
+  // todo use regex => tab.title
+  chrome.tabs.query({}, (tabs) => {
+    for (const t of tabs) {
+      if (!t.title.toLowerCase().includes("foundry virtual tabletop")) {
+        continue;
+      }
+      chrome.tabs.update(t.id, { url: t.url });
+    }
+  });
+};
